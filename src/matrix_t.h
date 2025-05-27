9d501e131370ae68fb4c06ae417c55f6887cbf25
Gleb Mikhailov <gmworklife25@gmail.com>
2025-05-27 14:18:57 +0300

    feat: Add s21_create_matrix func

--- src/matrix_t.h
+++ src/matrix_t.h
@@ -7,4 +7,13 @@ typedef struct matrix_struct {
   int columns;
 } matrix_t;
 
+#include <stddef.h>
+#define EMPTY_MATRIX_T \
+  { NULL, 0, 0 }
+#define TEMPORARY_EMPTY_MATRIX_T (matrix_t) EMPTY_MATRIX_T
+
+int s21_create_matrix(int rows, int columns, matrix_t* result);
+
+int s21_is_matrix_params_valid(int rows, int columns, const matrix_t* matrix);
+
 #endif  // MATRIX_T_H
---
+++ src/matrix_t/matrix_t.c
@@ -0,0 +1,29 @@
+#include "matrix_t.h"
+
+#include <stddef.h>
+
+#include "common.h"
+#include "matrix_t/matrix_operation_status_code_t.h"
+
+int s21_create_matrix(int rows, int columns, matrix_t* result) {
+  if (!s21_is_matrix_params_valid(rows, columns, result)) {
+    return INVALID_MATRIX;
+  }
+
+  matrix_operation_status_code_t status_code = OK;
+  result->matrix =
+      s21_safe_malloc((size_t)rows * sizeof(double*) +
+                      (size_t)rows * (size_t)columns * sizeof(double));
+  if (result->matrix != NULL) {
+    double* ptr = (double*)(result->matrix + rows);
+    for (int i = 0; i < rows; ++i) {
+      result->matrix[i] = ptr + columns * i;
+    }
+    result->rows = rows;
+    result->columns = columns;
+  } else {
+    status_code = INVALID_MATRIX;
+  }
+
+  return status_code;
+}
