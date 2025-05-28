cbd4c0dce51a80ceb36b7144feb4da2c493a7141
Gleb Mikhailov <gmworklife25@gmail.com>
2025-05-28 10:47:26 +0300

    feat: Add s21_mult_number func

--- src/matrix_t/arithmetic.c
+++ src/matrix_t/arithmetic.c
@@ -37,3 +37,18 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
 
   return OK;
 }
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
+  if (!s21_is_matrix_valid(A) || !result) {
+    return INVALID_MATRIX;
+  }
+
+  s21_create_matrix(A->rows, A->columns, result);
+  for (int i = 0; i < result->rows; ++i) {
+    for (int j = 0; j < result->columns; ++j) {
+      result->matrix[i][j] = number * A->matrix[i][j];
+    }
+  }
+
+  return OK;
+}
--- src/matrix_t/arithmetic.h
+++ src/matrix_t/arithmetic.h
@@ -7,4 +7,6 @@ int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+
 #endif  // MATRIX_T_ARITHMETIC_H
